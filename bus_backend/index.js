b3b18655233e8e4936b4b142e8dbfd94c487f0e3
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-11-23 22:33:40 +0530

    Added CORS configuration with two separate options and middleware functions.

--- bus_backend/index.js
+++ bus_backend/index.js
@@ -31,7 +31,17 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
 app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.options('*', cors(corsOptions2));
 
 
 
