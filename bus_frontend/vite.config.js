cdca3ed95b323a23630e8f6b8c5b2a3e1d2d25f2
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-11-23 23:17:22 +0530

    "Added Content-Security-Policy headers to vite.config.js"

--- bus_frontend/vite.config.js
+++ bus_frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; img-src 'self' data:; connect-src 'self'; script-src 'self'; style-src 'self';",
+    },
+  },
+
 })
