b18e26e212e2f59bbbf27b943f367b153d1cf2cd
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-11-23 21:31:06 +0530

    "Added CORS headers and options route to enable cross-origin requests"

--- bus_backend/index.js
+++ bus_backend/index.js
@@ -17,6 +17,14 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://gomap-nine.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.options('*', cors(corsOptions));
+
 
 
 app.use(bodyParser.json());
